560149fda2bb0938cf956e7582847bfb97b0f7e7
Henriette <henelt@outlook.com>
2023-08-15 23:00:50 +0200

    refactor: user able to specify file size

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 from pathlib import Path
 
 fi = open("testingfile.txt", "a")
+
+
 def create_random_sentences(filesize):
     for i in range(0, filesize):
-        if Path("testingfile.txt").stat().st_size != (i+1):
+        if Path("testingfile.txt").stat().st_size != (i + 1):
             fi.write("a")
 
 
 if __name__ == '__main__':
     open(fi.name, "w").close()
-    create_random_sentences(20000)
+
+    while True:
+        filesize = input("File size: ")
+        if filesize.isnumeric():
+            break
+        else:
+            print("The value needs to be a number!")
+    create_random_sentences(int(filesize))
+    print("Successful input")
